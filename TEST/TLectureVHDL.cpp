896fd7c36ae70454e89301aeb70a5a495be7c8c3
matescou <matescou@neuf.fr>
2016-11-30 09:55:01 +0100

    Creation d'une fonction pour mettre les chaines de caracteres en minuscule et d'une seconde pour extraire les lexemes

--- TEST/TLectureVHDL.cpp
+++ TEST/TLectureVHDL.cpp
@@ -1,6 +1,48 @@
+#include <fstream>
+#include <iostream>
+
 using namespace std;
+
+string toMinuscule(string word);
+void extractLexeme();
 
 int main()
 {
 
+}
+
+void extractLexeme ()
+{
+	ifstream monFlux("../VHDL/ALU.vhd");
+
+	if(monFlux)
+	{
+		string word;
+		while (monFlux.eof() != 1)
+		{
+			monFlux >> word;
+			cout << word << endl;	
+		}		
+		
+	}
+	else
+	{
+		cout << "ERROR: File not found" << endl;
+	}
+}
+
+string toMinuscule(string word)
+{	
+	for (int i = 0; i < word.size(); i++)
+	{
+		for (int j = 0; j < 26; j++)
+		{
+			if (word[i] == j+65)
+			{
+				word[i] = j+97;
+			}	
+		}
+	}
+	
+	return word;
 }
