e2757344b685917ff3b4a63629031689190fdd09
phelma <phelma@localhost.localdomain>
2017-01-31 19:17:04 +0100

    Ajout de tous les tests

--- TEST/FINAL_TEST/start_tests.bash
+++ TEST/FINAL_TEST/start_tests.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PATH_TEST='TEST/FINAL_TEST/'
-LIST_TEST_FILES='archivide assign biblio case'
+LIST_TEST_FILES='archivide assign base_bit base_ports base biblio case entite_noarchi entite_seule erreur file_list instances latch process_clk2 process_clk3 process_clk4 process_clk proc proc_vide sensibilite signaux typage variables vide'
 RESULTS_DIRECTORY='RESULTS/'
 
 cd ../../
@@ -12,4 +12,5 @@ do
 	./main -tree -debug -file $PATH_TEST$files.vhd |tee $PATH_TEST$RESULTS_DIRECTORY$files.log
 done
 
+grep -c ERR TEST/FINAL_TEST/RESULTS/*
 cd TEST/FINAL_TEST/
